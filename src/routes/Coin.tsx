743d7608d465e13e246151bba94c02cbb4c41b62
pjk0007 <44570338+pjk0007@users.noreply.github.com>
2021-11-22 22:57:04 +0900

    get info of coins from API

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,116 @@
+import { useLocation, useParams } from 'react-router';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+
+const Container = styled.div`
+    padding: 0 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 15vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    font-size: 40px;
+    color:${props=>props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align:center;
+    display: block;
+`;
+
+interface RouteParams{
+    coinId: string;
+}
+
+interface IInfoData{
+    id:string;
+    name:string;
+    symbol:string;
+    rank:number;
+    is_new:boolean;
+    is_active:boolean;
+    type:string;
+    contract:string;
+    platform:string;
+    description:string;
+    message:string;
+    open_source:boolean;
+    started_at:string;
+    development_status:string;
+    hardware_wallet:boolean;
+    proof_type:string;
+    org_structure:string;
+    hash_algorithm:string;
+    first_data_at:string;
+    last_data_at:string;
+}
+
+interface IPriceData{
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD:{
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        }
+    };
+}
+
+//https://api.coinpaprika.com/v1/coins/${coinId}
+//https://api.coinpaprika.com/v1/tickers/${coinId}
+function Coin(){
+    const [loading, setLoading] = useState(true);
+    const { coinId } = useParams() as RouteParams;
+    const { state } = useLocation();
+    const [info, setInfo] = useState<IInfoData>();
+    const [priceInfo, setPriceInfo] = useState<IPriceData>();
+    useEffect(() => {
+        (async () =>{
+            const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+            const priceData = await(await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, [])
+    
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading"}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : priceInfo?.quotes.USD}
+        </Container>
+    );
+}
+
+export default Coin;
