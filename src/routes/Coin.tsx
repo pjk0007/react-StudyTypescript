0a864df31f152da5b173ae4adede3d06c261ae9d
pjk0007 <44570338+pjk0007@users.noreply.github.com>
2021-12-10 00:36:31 +0900

    deploy setting, bug fixed

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -163,7 +163,7 @@ function Coin() {
         refetchInterval: 5000,
       }
     );
-  const priceData = tickersData?.quotes.USD;
+  const priceData = tickersData?.quotes?.USD;
   const loading = infoLoading || tickersLoading;
   return (
     <Container>
@@ -196,7 +196,7 @@ function Coin() {
             </OverviewItem>
             <OverviewItem>
               <span>Price:</span>
-              <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
+              <span>${tickersData?.quotes?.USD.price.toFixed(3)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
